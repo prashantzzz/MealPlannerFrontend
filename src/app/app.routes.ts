0c26735216559857016e5183853cc30a07f3d575
Prashant <prashant2003prashant@gmail.com>
2024-12-08 18:31:29 +0530

    fixed preference

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MealplanComponent } from './mealplan/mealplan.component';
+import { PreferenceComponent } from './preference/preference.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'mealplan', component: MealplanComponent },
+  { path: 'preferences', component: PreferenceComponent },
   { path: 'recipes', component: RecipeComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' } // Redirect to /login by default
 ];
